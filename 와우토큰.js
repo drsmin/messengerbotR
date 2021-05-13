2aff58b90bae86b8b601ff67d007947c8e39dffa
drsmin <33747419+drsmin@users.noreply.github.com>
2021-05-13 18:18:44 +0900

    Create 와우토큰.js

---
+++ 와우토큰.js
@@ -0,0 +1,41 @@
+const scriptName = "와우토큰";
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    var cmd = msg.split(" ")[0];
+
+    if (cmd == "/와우토큰") {
+        try {
+        
+          var data = org.jsoup.Jsoup.connect("https://wowtokenprices.com/Korea")
+              .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
+              .get().select("#money-text").get(0).text();
+          replier.reply("와우 토큰 시세: " + data);
+        } catch (e) {
+          replier.reply("와우토큰 정보 불러오기 실패");
+        }
+    }
+}
+
+//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+function onCreate(savedInstanceState, activity) {
+  var textView = new android.widget.TextView(activity);
+  textView.setText("Hello, World!");
+  textView.setTextColor(android.graphics.Color.DKGRAY);
+  activity.setContentView(textView);
+}
+
+function onStart(activity) {}
+
+function onResume(activity) {}
+
+function onPause(activity) {}
+
+function onStop(activity) {}
